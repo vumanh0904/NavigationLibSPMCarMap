1c26a368dd193c03bce7ac6f77934d299666feeb
imap Mac <goongimap@icloud.com>
2024-07-18 14:23:54 +0700

    update

--- Package.swift
+++ Package.swift
@@ -12,15 +12,8 @@ let package = Package(
             targets: ["NavigationLibSPMCarMap"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NavigationLibSPMCarMap"),
-        .testTarget(
-            name: "NavigationLibSPMCarMapTests",
-            dependencies: ["NavigationLibSPMCarMap"]),
         .binaryTarget(
-            name: "NavigationSDKFramework",
+            name: "NavigationLibSPMCarMap",
             url: "https://github.com/vumanh0904/NavigationLibSPMCarMap/releases/download/1.0.1/NavigationSDKFramework.xcframework.zip",
             checksum: "4b0f34f0f36f1fc4eba2c0f4e21680a1c4c11f33d4576752fd2494d870facb46"
         )
