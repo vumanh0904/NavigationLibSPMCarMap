d23a0003821dff6e839e3bd9b585d095d9eda5f7
imap Mac <goongimap@icloud.com>
2024-07-18 17:33:16 +0700

    update

--- Package.swift
+++ Package.swift
@@ -1,21 +1,36 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+//import PackageDescription
+//
+//let package = Package(
+//    name: "NavigationLibSPMCarMap",
+//    products: [
+//        // Products define the executables and libraries a package produces, making them visible to other packages.
+//        .library(
+//            name: "NavigationLibSPMCarMap",
+//            targets: ["NavigationLibSPMCarMap"]),
+//    ],
+//    targets: [
+//        .binaryTarget(
+//            name: "NavigationLibSPMCarMap",
+//            url: "https://github.com/vumanh0904/NavigationLibSPMCarMap/releases/download/1.0.3/NavigationSDKFramework.xcframework.zip",
+//            checksum: "4b0f34f0f36f1fc4eba2c0f4e21680a1c4c11f33d4576752fd2494d870facb46"
+//        )
+//    ]
+//)
 import PackageDescription
 
 let package = Package(
     name: "NavigationLibSPMCarMap",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NavigationLibSPMCarMap",
-            targets: ["NavigationLibSPMCarMap"]),
+            targets: ["VuManhFramework"]),
     ],
+    dependencies: [],
     targets: [
-        .binaryTarget(
-            name: "NavigationLibSPMCarMap",
-            url: "https://github.com/vumanh0904/NavigationLibSPMCarMap/releases/download/1.0.3/NavigationSDKFramework.xcframework.zip",
-            checksum: "4b0f34f0f36f1fc4eba2c0f4e21680a1c4c11f33d4576752fd2494d870facb46"
-        )
+        .binaryTarget(name: "VuManhFramework", path: "./Sources/VuManhFramework.xcframework")
     ]
 )
---
+++ Sources/VuManhFramework.xcframework/ios-arm64/VuManhFramework.framework/Headers/VuManhFramework.h
@@ -0,0 +1,18 @@
+//
+//  VuManhFramework.h
+//  VuManhFramework
+//
+//  Created by macos on 18/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VuManhFramework.
+FOUNDATION_EXPORT double VuManhFrameworkVersionNumber;
+
+//! Project version string for VuManhFramework.
+FOUNDATION_EXPORT const unsigned char VuManhFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VuManhFramework/PublicHeader.h>
+
+
