86b5def4d15484994bb50f8a6cf537f9a541f901
imap Mac <goongimap@icloud.com>
2024-07-18 11:40:20 +0700

    update

--- Package.swift
+++ Package.swift
@@ -20,8 +20,9 @@ let package = Package(
             name: "NavigationLibSPMCarMapTests",
             dependencies: ["NavigationLibSPMCarMap"]),
         .binaryTarget(
-            name: "NavigationSDKFramework",
-            url: "https://github.com/vumanh0904/NavigationLibSPMCarMap/releases/download/1.0.1/NavigationSDKFramework.xcframework.zip",
-        ),
+                    name: "NavigationSDKFramework",
+                    url: "https://github.com/vumanh0904/NavigationLibSPMCarMap/releases/download/1.0.1/NavigationSDKFramework.xcframework.zip", // Replace with the actual URL
+                    checksum: "1111111"  // Replace with the actual checksum
+                )
     ]
 )
