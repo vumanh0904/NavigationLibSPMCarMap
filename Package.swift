833973adf667a3fb020802866f7ff53b3a731734
imap Mac <goongimap@icloud.com>
2024-07-19 14:35:59 +0700

    update

--- Package.swift
+++ Package.swift
@@ -27,10 +27,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NavigationLibSPMCarMap",
-            targets: ["VuManhFramework"]),
+            targets: ["NavigationSDKFramework"]),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "VuManhFramework", path: "./Sources/VuManhFramework.xcframework")
+        .binaryTarget(name: "NavigationSDKFramework", path: "./Sources/NavigationSDKFramework.xcframework")
     ]
 )
---
+++ Sources/NavigationSDKFramework.xcframework/ios-arm64/NavigationSDKFramework.framework/Headers/NavigationSDKFramework.h
@@ -0,0 +1,18 @@
+//
+//  NavigationSDKFramework.h
+//  NavigationSDKFramework
+//
+//  Created by macos on 17/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NavigationSDKFramework.
+FOUNDATION_EXPORT double NavigationSDKFrameworkVersionNumber;
+
+//! Project version string for NavigationSDKFramework.
+FOUNDATION_EXPORT const unsigned char NavigationSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationSDKFramework/PublicHeader.h>
+
+
